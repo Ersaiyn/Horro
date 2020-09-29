35439e20e3ec52e66119a50aba4341f295d6c8a0
Ersaiyn <72074874+Ersaiyn@users.noreply.github.com>
2020-09-29 17:03:16 +0600

    Neew project

---
+++ Assets/Scripts/FlashLight.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour
+{
+
+    Light spotLight;
+    AudioSource audio;
+    public AudioClip bip;
+	
+    void Start()
+    {
+        spotLight = GetComponent<Light>();
+        audio = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+	{
+	    if(spotLight.enabled == false)
+	    {
+		spotLight.enabled = true;
+                audio.PlayOneShot(bip);
+	    }
+	    else
+	    {
+		spotLight.enabled = false;
+                audio.PlayOneShot(bip);
+	    }
+	}
+    }
+}
