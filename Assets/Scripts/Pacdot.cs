688bc789188b08f29a97abd52fddbc51b3f2c861
Ersaiyn <72074874+Ersaiyn@users.noreply.github.com>
2020-10-16 15:58:14 +0600

    exam

---
+++ Assets/Scripts/Pacdot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pacdot : MonoBehaviour
+{
+    public Text scoreText;
+    public int score = 0;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D co){
+        if (co.name == "Pacman"){
+            score += 10;
+            scoreText.text += score; 
+            Destroy(gameObject);
+        }
+    }
+}
